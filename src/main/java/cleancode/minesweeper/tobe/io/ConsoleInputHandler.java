9e719c3ed605daee31532b522c84ece14aac0fab
안치숙 <songweddin@naver.com>
2025-03-07 23:08:55 +0900

    [4-6] 도메인 개념 도출하기
    GameConfig 객체를 만들어서 설정 정보들 저장할 수 있는 객체 만들기
    게임 실행할 때는 GameConfig 객체 활용

--- src/main/java/cleancode/minesweeper/tobe/io/ConsoleInputHandler.java
+++ src/main/java/cleancode/minesweeper/tobe/io/ConsoleInputHandler.java
@@ -14,9 +14,9 @@ public class ConsoleInputHandler implements InputHandler{
     public CellPosition getCellPositionFromUser() {
         String userInput = SCANNER.nextLine();
 
-        int colIndex = boardIndexConverter.getSelectedColIndex(userInput);
         int rowIndex = boardIndexConverter.getSelectedRowIndex(userInput);
-        return CellPosition.of(colIndex, rowIndex);
+        int colIndex = boardIndexConverter.getSelectedColIndex(userInput);
+        return CellPosition.of(rowIndex, colIndex);
     }
     @Override
     public UserAction getUserActionFromUser() {
