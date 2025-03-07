ad07d4cfb5e87dad6be56c125c1954c19fe0f4b2
안치숙 <songweddin@naver.com>
2025-03-07 17:05:26 +0900

    [4-2] Value Object
    행, 열 나타내는 좌표를 CellPosition VO 객체로 나타냄
    사용자 좌표 받아올 때 CellPosition 반환
    좌표 검증 위해 converter에서는 0미만만 검증, size 정보 가지고 있는 gameBoard에서 사이즈 범위 벗어난 것 검증
    주변 좌표 사용 위해 RelativePosition 클래스 생성, 3*3 좌표들 객체 선언
    CellPosition 객체에서 RelativePosition과 관련된 연산 수행

--- src/main/java/cleancode/minesweeper/tobe/io/ConsoleInputHandler.java
+++ src/main/java/cleancode/minesweeper/tobe/io/ConsoleInputHandler.java
@@ -1,11 +1,25 @@
 package cleancode.minesweeper.tobe.io;
 
+import cleancode.minesweeper.tobe.BoardIndexConverter;
+import cleancode.minesweeper.tobe.position.CellPosition;
+
 import java.util.Scanner;
 
 public class ConsoleInputHandler implements InputHandler{
     public static final Scanner SCANNER = new Scanner(System.in);
+    private final BoardIndexConverter boardIndexConverter = new BoardIndexConverter();
+
+    @Override
+    public CellPosition getCellPositionFromUser() {
+        String userInput = SCANNER.nextLine();
+
+        int colIndex = boardIndexConverter.getSelectedColIndex(userInput);
+        int rowIndex = boardIndexConverter.getSelectedRowIndex(userInput);
+        return CellPosition.of(colIndex, rowIndex);
+    }
+
     @Override
     public String getUserInput() {
-        return SCANNER.nextLine();
+        return SCANNER.nextLine()  ;
     }
 }
